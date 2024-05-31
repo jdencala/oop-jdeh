7a356bf2b5ca1a03020a3b085ee1cdb8fe120f8a
jdencala <jdencalahuaya@gmail.com>
2024-05-31 12:16:38 -0500

    Add(III): Add printSquare method

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,27 @@ public class Main {
         //Examples for printLine method
         printLine(5);
         printLine(10);
-        printLine(15);
+        //Examples for pintSquare method
+        printSquare(6);
+        printSquare(4);
     }
     public static void printLine(int n){
+        System.out.println("----------------- Printing Line (" +  n  +")----------------");
+        System.out.println(" ");
         for(int i = 0; i < n; i++) {
             System.out.print("* ");
         }
         System.out.println(" ");
+    }
+
+    public static void printSquare(int n) {
+        System.out.println("----------------- Printing Square (" +  n  +")----------------");
+        System.out.println(" ");
+        for(int h = 0; h < n; h++) {
+            for(int w = 0; w < n; w++) {
+                System.out.print(" * ");
+            }
+            System.out.println(" ");
+        }
     }
 }
