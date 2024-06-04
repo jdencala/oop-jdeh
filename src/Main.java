f637c02821682874042550b96bf5a14e62f6bb8a
jdencala <jdencalahuaya@gmail.com>
2024-06-04 13:09:45 -0500

    Add(I): Add Menu Figures

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,59 @@ import figures.Line;
 import figures.Rectangle;
 import figures.Square;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //Examples for printLine method
-        Line line1 = new Line(5);
-        line1.printLine();
-        Line line2 = new Line(10);
-        line2.printLine();
-        //Examples for pintSquare method
-        Square square1 = new Square(6);
-        square1.printSquare();
-        Square square2 = new Square(4);
-        square2.printSquare();
-        //Examples for print rectangle method
-        Rectangle rectangle = new Rectangle(6, 4);
-        rectangle.printRectangle();
+        int option = 0;
+        do{
+            System.out.println("1. Create Line");
+            System.out.println("2. Create Square");
+            System.out.println("3. Create Rectangle");
+            System.out.println("4. Exit");
+            Scanner scanner = new Scanner(System.in);
+            option = scanner.nextInt();
+            switch (option) {
+                case 1:
+                    createLine();
+                    break;
+                case 2:
+                    createSquare();
+                    break;
+                case 3:
+                    createRectangle();
+                    break;
+                case 4:
+                    System.out.println("----Exit System----");
+                        break;
+            }
+
+        } while(option != 4);
+    }
+
+    public static void createRectangle() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a width for Rectangle: ");
+        int width = sc.nextInt();
+        System.out.println("Please enter a height for Rectangle: ");
+        int height = sc.nextInt();
+        Rectangle rc = new Rectangle(width, height);
+        rc.printRectangle();
+    }
+
+    public static void createSquare() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a length for Square: ");
+        int squareLength = sc.nextInt();
+        Square sq = new Square(squareLength);
+        sq.printSquare();
+    }
+
+    public static void createLine() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a length for line: ");
+        int lineLength = sc.nextInt();
+        Line line = new Line(lineLength);
+        line.printLine();
     }
 }
