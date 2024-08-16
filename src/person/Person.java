dfe5966629efbfedaa887b7ce58b53dfec3aa99d
jdencala <jdencalahuaya@gmail.com>
2024-08-16 11:38:21 -0500

    Add(X): Exception Examples

---
+++ src/person/Person.java
@@ -0,0 +1,49 @@
+package person;
+
+public class Person {
+    private String name;
+    private String lastname;
+    private Integer age;
+
+    public Person() {
+    }
+
+    public Person(String name, String lastname, Integer age) {
+        this.name = name;
+        this.lastname = lastname;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
